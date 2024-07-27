c6396c444b36b6f1206a89a19c26ed19fba1dfb7
Asad <asadmehm00d961@gmail.com>
2024-07-28 01:20:14 +0500

    First Commit

---
+++ src/main/java/com/scm/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/home")
+    public String home(){
+        System.out.println("Home Page handler");
+        return "home";
+    }
+
+}
