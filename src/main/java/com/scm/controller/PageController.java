bfedc7890986ca3de5d361cbad8f188eec7c32a4
Asad <asadmehm00d961@gmail.com>
2024-08-01 23:03:44 +0500

    First Commit

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home() {
         System.out.println("Home Page handler");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        System.out.println("About Page handler");
+        return "about";
     }
 
 }
