8a0a8d2b6e14d7f8c4f3fa64232ca8284aca2fa3
Asad <asadmehm00d961@gmail.com>
2024-08-08 01:36:15 +0500

    Navbar Updated

--- src/main/java/com/scm/controller/PageController.java
+++ src/main/java/com/scm/controller/PageController.java
@@ -12,10 +12,33 @@ public class PageController {
         return "home";
     }
 
+    @GetMapping("/services")
+    public String services() {
+        System.out.println("Services Page handler");
+        return "services";
+    }
+
     @GetMapping("/about")
     public String about() {
         System.out.println("About Page handler");
         return "about";
     }
 
+    @GetMapping("/contact")
+    public String contact() {
+        System.out.println("Contact Page handler");
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        System.out.println("Login Page handler");
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        System.out.println("Register Page handler");
+        return "register";
+    }
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,59 @@
+console.log("Script loaded");
+
+let currentTheme = getTheme();
+// Initial stage
+document.addEventListener("DOMContentLoaded", () =>{
+    changeTheme();
+});
+
+
+//TODO:
+function changeTheme() {
+// Set current theme to WebPage
+    changePageTheme(currentTheme, currentTheme);
+    // Set the listener to change theme button
+    const changeThemeButton = document.querySelector("#theme_change_button");
+    changeThemeButton.querySelector("span").textContent =
+        currentTheme == "light" ? "Dark" : "Light";
+    const oldTheme = currentTheme
+    changeThemeButton.addEventListener("click", (e) => {
+        console.log("Button clicked");
+        if (currentTheme === "dark") {
+            //change to light
+            currentTheme = "light"
+        } else {
+            //   change to dark
+            currentTheme = "dark";
+        }
+        changePageTheme(currentTheme, oldTheme);
+    });
+}
+
+//Set theme to LocalStorage
+function setTheme(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+//Get theme from LocalStorage
+function getTheme() {
+    let theme = localStorage.getItem("theme");
+    return theme ? theme : "light";
+    // if (theme)
+    //     return theme
+    // else return "light";
+}
+
+//change current page theme
+function changePageTheme(theme, oldTheme) {
+    //update to local storage
+    setTheme(currentTheme);
+// remove the current theme
+    document.querySelector("html").classList.remove(oldTheme);
+//set the current theme
+    document.querySelector("html").classList.add(theme);
+
+    //     change the color of button
+    document
+        .querySelector("#theme_change_button")
+        .querySelector("span").textContent = theme == "light" ? "dark" : "light";
+}
